c03288b6d2e9ee9bd9a13568160a8abf8cefc954
jopnot <Jopklein4@gmail.com>
2024-12-13 11:59:19 +0100

    uhhh kevers

---
+++ Assets/Scripts/enemy-movement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemymovement : MonoBehaviour
+{
+     private UnityEngine.AI.NavMeshAgent agent;
+    private Transform playerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      // Vind de NavMeshAgent component
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+        // Zoek de XR Rig met de tag "Player"
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            playerTransform = player.transform;
+        }   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         // Beweeg naar de positie van de speler als deze is gevonden
+        if (playerTransform != null)
+        {
+            agent.SetDestination(playerTransform.position);
+        } 
+    }
+}
